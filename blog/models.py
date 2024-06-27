8fc4bde589ef98fc3b7960d4156b50dd2ad8391f
Volodya132 <56843184+Volodya132@users.noreply.github.com>
2024-06-27 16:29:42 +0300

    l

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,25 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
     published_date = models.DateTimeField()
+    author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL, related_name='posts')
 
     def __str__(self):
         return self.title
+
+    def published_recently(self):
+        return self.published_date >= timezone.now() - timedelta(days=7)
+
+    published_recently.boolean = True
+    published_recently.short_description = 'Published Recently?'
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Post
+from .models import Post, Author
+
 
 def post_list(request):
     posts = Post.objects.all().order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def posts_by_author(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    posts = Post.objects.filter(author=author).order_by('-published_date')
+    return render(request, 'blog/posts_by_author.html', {'author': author, 'posts': posts})
